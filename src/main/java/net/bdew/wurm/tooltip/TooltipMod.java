046e4c517cf5c287b016b30e26ae451bd1c18381
bdew <blackdew@gmail.com>
2017-06-02 12:31:31 +0300

    added horse colors, sex for all animals, and option to show harvestable status on infected trees/bushes

--- src/main/java/net/bdew/wurm/tooltip/TooltipMod.java
+++ src/main/java/net/bdew/wurm/tooltip/TooltipMod.java
@@ -2,6 +2,7 @@ package net.bdew.wurm.tooltip;
 
 import javassist.ClassPool;
 import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
+import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
 import org.gotti.wurmunlimited.modloader.interfaces.Initable;
 import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmMod;
@@ -10,15 +11,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class TooltipMod implements WurmMod, Initable, PreInitable {
+public class TooltipMod implements WurmMod, Initable, PreInitable, Configurable {
     private static final Logger logger = Logger.getLogger("TooltipMod");
+
+    public static boolean showSpecialTreesHarvestable;
 
     public static void logException(String msg, Throwable e) {
         if (logger != null)
             logger.log(Level.SEVERE, msg, e);
+    }
+
+    @Override
+    public void configure(Properties properties) {
+        showSpecialTreesHarvestable = Boolean.parseBoolean(properties.getProperty("showSpecialTreesHarvestable", "false"));
     }
 
     private String loadString(String id) {
@@ -31,7 +40,9 @@ public class TooltipMod implements WurmMod, Initable, PreInitable {
                 builder.append(line);
                 builder.append(System.lineSeparator());
             }
-            return builder.toString();
+            String res = builder.toString();
+            res = res.replace("$showSpecialTreesHarvestable", Boolean.toString(showSpecialTreesHarvestable));
+            return res;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -50,6 +61,10 @@ public class TooltipMod implements WurmMod, Initable, PreInitable {
             classPool.getCtClass("com.wurmonline.client.renderer.cave.CaveWallPicker")
                     .getMethod("getHoverName", "()Ljava/lang/String;")
                     .setBody(loadString("caveWallPicker.txt"));
+
+            classPool.getCtClass("com.wurmonline.client.renderer.cell.CreatureCellRenderable")
+                    .getMethod("getHoverDescription", "(Lcom/wurmonline/client/renderer/PickData;)V")
+                    .insertAfter(loadString("creaturePicker.txt"));
 
 
             logger.fine("Loaded");
